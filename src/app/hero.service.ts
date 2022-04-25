86f2d9105f7a3025fe1e724dc512ea49506b7a0a
Ebo Arcadia <yiboli.coding@gmail.com>
2022-04-24 21:26:20 -0400

    add observerable module, using subscribe method in hero service to async assign heroes from remote resource to local

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heros'
+import { HEROES } from './mock-heros';
+import { Observable, of } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
   constructor() { }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,7 +23,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
 }
