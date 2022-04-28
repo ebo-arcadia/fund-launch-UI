f42c29069069a4975de3b650f6efd114d1f7dcaf
Ebo Arcadia <yiboli.coding@gmail.com>
2022-04-27 20:17:49 -0400

    add feature msgs to be rendered on the UI when hero is clicked, clear msg with clear button, using messgae service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import { Hero } from '../hero'
 // import { HEROES } from '../mock-heros'
 import { HeroService} from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -15,13 +16,18 @@ export class HeroesComponent implements OnInit {
   }
   heroes: Hero[] = [];
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-}
-  constructor(private heroService: HeroService) { }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+}
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
