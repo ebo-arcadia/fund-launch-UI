aa5b7c7c7e07e6e9fb12985eed206d6a1fbdd38f
Ebo Arcadia <yiboli.coding@gmail.com>
2022-04-30 10:35:00 -0400

    extract param value from url, store in private attributes using imported angular services, construct get hero method to render hero data from remote using hero service component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,5 +13,13 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    // handle errors?
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
   constructor(private messageService: MessageService) { }
 }
