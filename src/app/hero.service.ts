f7a69d7b29ad7f7fecc2496683e45b4aa4e33cff
Ebo Arcadia <yiboli.coding@gmail.com>
2022-05-02 09:39:13 -0400

    add create hero feature, hero can be persisted to the mock server, using post() with server auto generate id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -78,4 +78,11 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`add hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    )
+  }
+
 }
